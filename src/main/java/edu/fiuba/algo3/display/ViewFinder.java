2cd7f9491fc85ac1b1775d16afac195e0e6f00ab
Javier Zardain <jzardain@fi.uba.ar>
2020-07-17 21:39:11 -0300

    Cambio en forma de buscar vistas

--- src/main/java/edu/fiuba/algo3/display/ViewFinder.java
+++ src/main/java/edu/fiuba/algo3/display/ViewFinder.java
@@ -1,14 +1,15 @@
 package edu.fiuba.algo3.display;
 
 import java.io.InputStream;
+import edu.fiuba.algo3.App;
 
 public class ViewFinder {
 	
-	private static final String VIEW_PACKAGE = "/edu/fiuba/algo3/view/";
+	private static final String VIEW_PACKAGE = "view/";
 	private static final String FXML_PREFIX = ".fxml";
 
 	public static InputStream findView(String viewName) {
-		return ViewFinder.class.getResourceAsStream(VIEW_PACKAGE + viewName + FXML_PREFIX);
+		return App.class.getResourceAsStream(VIEW_PACKAGE + viewName + FXML_PREFIX);
 	}
 	
 }
