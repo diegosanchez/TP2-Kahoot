83b91ce69932ec35be066b90c13b9c0f84d1a28d
Javier Zardain <jzardain@fi.uba.ar>
2020-07-18 02:36:37 -0300

    Fix carga de views

--- src/main/java/edu/fiuba/algo3/display/ViewFinder.java
+++ src/main/java/edu/fiuba/algo3/display/ViewFinder.java
@@ -4,12 +4,12 @@ import java.io.InputStream;
 import edu.fiuba.algo3.App;
 
 public class ViewFinder {
-	
-	private static final String VIEW_PACKAGE = "view/";
+
+	private static final String VIEW_PACKAGE = "/view/";
 	private static final String FXML_PREFIX = ".fxml";
 
 	public static InputStream findView(String viewName) {
 		return App.class.getResourceAsStream(VIEW_PACKAGE + viewName + FXML_PREFIX);
 	}
-	
+
 }
