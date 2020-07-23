15148de793c891ba90b14b6740ccfe02f57d644c
Javier Zardain <jzardain@fi.uba.ar>
2020-07-24 00:17:13 +0200

    Movidas views y hojas de estilo a src/main/resources Cargador de hojas de estilo

--- src/main/java/edu/fiuba/algo3/constants/ResourceConstants.java
+++ src/main/java/edu/fiuba/algo3/constants/ResourceConstants.java
@@ -4,7 +4,9 @@ public class ResourceConstants {
 	
 	public static final String RESOURCES_ROOT_PATH = "/";
 	public static final String QUESTIONS_PATH = "questions/preguntas.json";
+	public static final String STYLES_PATH = "styles/";	
 	public static final String QUESTIONS_TEST_PATH = "questions/preguntas-test.json";
 	public static final String STYLES_ROOT_PATH = "src/main/java/edu/fiuba/algo3/styles/";
+	
 }
 
---
+++ src/main/java/edu/fiuba/algo3/constants/Stylesheets.java
@@ -0,0 +1,6 @@
+package edu.fiuba.algo3.constants;
+
+public class Stylesheets {
+	public static final String QUESTIONS_CSS = "questions";
+
+}
