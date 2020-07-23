50b030c4a65199da7a8da1f035e55cfe2f5f5efd
Miroslav <psapunar@fi.uba.ar>
2020-07-23 18:50:21 -0300

    Control de visualización botones de respuesta

--- src/main/java/edu/fiuba/algo3/controller/GenericQuestionController.java
+++ src/main/java/edu/fiuba/algo3/controller/GenericQuestionController.java
@@ -6,25 +6,30 @@ import edu.fiuba.algo3.exceptions.FileNotFoundException;
 import edu.fiuba.algo3.exceptions.ViewLoadingException;
 import edu.fiuba.algo3.loaders.SceneLoader;
 import edu.fiuba.algo3.model.Game;
+import edu.fiuba.algo3.model.GameOption;
 import edu.fiuba.algo3.model.Player;
 
 import edu.fiuba.algo3.model.Question;
+import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import edu.fiuba.algo3.resources.ResourceFinder;
 import edu.fiuba.algo3.resources.ResourceLoader;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.util.Iterator;
 import java.io.File;
+import java.util.List;
 
 
 public class GenericQuestionController {
@@ -34,7 +39,9 @@ public class GenericQuestionController {
     @FXML
     public Label playerScore;
     @FXML
-    public Text questionText;
+    public GridPane gridPane;
+    @FXML
+    public Label questionText;
     @FXML
     public Label questionType;
     @FXML
@@ -66,13 +73,27 @@ public class GenericQuestionController {
         playerName.setText(((localGame.getCurrentPlayer()).getName()));
         //playerScore.setText(String.valueOf((localGame.getCurrentPlayer()).getScore()));
     }
-    private void setSceneQuestion(){
+    private void setSceneQuestion() {
+        Iterator<GameOption> iteratorOptions = question.getOptions().iterator();
+        GameOption gameOption = iteratorOptions.next();
+
         //questionType.setText(String.valueOf(question.getType()));
         questionText.setText(question.getText());
+        List<Button> buttonList = (List) gridPane.getChildren();
+
+        for (Button button : buttonList) {
+            button.setVisible(false);
+        }
+        int i = 0;
+        for (GameOption option : (question.getOptions())) {
+            buttonList.get(i).setVisible(true);
+            buttonList.get(i).setText(option.getText());
+            i++;
+        }
     }
 
     public void doAbandon(ActionEvent event) {
-        localGame.getCurrentPlayer().setScore(-1);//cuando anden los puntaje settear a 0 o mandar al controler de ganador el el otro player
+        localGame.getCurrentPlayer().setScore(-1);//cuando anden los puntajes settear a 0 o mandar al ResultsControler el otro player
         endGame();
     }
 
@@ -96,7 +117,6 @@ public class GenericQuestionController {
                 doSumitPlayer2(actionEvent);
             }
         });
-
     }
 
     private void showCorrectAnswer(){
