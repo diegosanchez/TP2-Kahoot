345f1fdbce74a1bbe69761e3c7bfce5c340eecb1
Javier Zardain <jzardain@fi.uba.ar>
2020-08-08 03:22:51 +0200

    Refactoring

--- src/main/java/edu/fiuba/algo3/model/Score.java
+++ src/main/java/edu/fiuba/algo3/model/Score.java
@@ -19,15 +19,7 @@ public class Score {
 	public void sum(Score score) {
 		value = score.getValue() + value;
 	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + value;
-		return result;
-	}
-
+	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
